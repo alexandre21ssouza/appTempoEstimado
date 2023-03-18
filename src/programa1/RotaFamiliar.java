246739ce84a95545a9bca408bf9ed08dd2e84dad
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-03-18 00:37:20 -0300

    Adição do Scanner e Locale região US.

--- src/programa1/RotaFamiliar.java
+++ src/programa1/RotaFamiliar.java
@@ -1,11 +1,16 @@
 package programa1;
 
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class RotaFamiliar {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		sc.close();
 	}
 
 	
