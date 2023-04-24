e8cffac05d2fd4b57c006c3308bc9d227e2123ae
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-04-24 20:01:58 -0300

    Inserindo o IF

--- src/programa1/RotaFamiliar.java
+++ src/programa1/RotaFamiliar.java
@@ -43,7 +43,60 @@ public class RotaFamiliar {
 		int casaTrabalho = 15;
 		int addEngarrafamento = 0;
 
+			if (despertador >= 6 && despertador <= 8) {
 
+			if (despertador <= 6) {
+				addEngarrafamento = 0;
+			} else if (despertador > 6) {
+				addEngarrafamento = 15; // minutos
+			} else if (despertador >= 7) { // hora
+				addEngarrafamento = 30; // minutos
+			}
+
+		} else if (despertador < 6 && aulaHoje.equals("sim")) {
+			System.out.println("As crianças vão chegar muito cedo para entrar na escola!");
+
+			System.out.println("Programe o despertador: (6 a 8 h)"); // horas
+			despertador = sc.nextInt();
+
+		} else {
+			System.out.println("Neste horário você vai se atrasar demais!");
+
+			System.out.println("Programe o despertador: (6 a 8 h)"); // horas
+			despertador = sc.nextInt();
+		}
+
+		if (trabalharHoje.equals("sim") && aulaHoje.equals("sim")) {
+//		
+			somaPercurso = vestirTrabalho + vestirCrianca + tomarCafeCrianca + casaEscola + escolaTrabalho
+					+ addEngarrafamento;
+
+		} else if (trabalharHoje.equals("sim") && aulaHoje.equals("não")) {
+			somaPercurso = vestirTrabalho + tomarCafeSolo + casaTrabalho + addEngarrafamento;
+
+		} else if (trabalharHoje.equals("não") && aulaHoje.equals("sim")) {
+			somaPercurso = vestirCrianca + tomarCafeCrianca + casaEscola + escolaCasa + addEngarrafamento;
+
+		}
+
+		int horas;
+		int minutos;
+
+		horas = ((somaPercurso / 60) % 24) + despertador;
+		minutos = somaPercurso % 60;
+
+		if (trabalharHoje.equals("não") && aulaHoje.equals("sim")) {
+			System.out.println("Vai Chegar em casa:  " + horas + " h " + " e " + minutos + " minutos ");
+
+		} else if (trabalharHoje.equals("não") && aulaHoje.equals("não")) {
+			System.out.println("Bom descanso para todos!");
+
+		} else {
+
+			System.out.println("Vai Chegar no trabalho:  " + horas + " h " + " e " + minutos + " minutos ");
+
+		
+		}
 
 
 		
